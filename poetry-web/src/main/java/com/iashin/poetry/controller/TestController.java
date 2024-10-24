7d87bcc7df80b19a0e1b4769722e0126e29f2bfc
dingzhen <dingzhen7186@163.com>
2024-10-24 18:02:06 +0800

    test:测试发邮件

--- poetry-web/src/main/java/com/iashin/poetry/controller/TestController.java
+++ poetry-web/src/main/java/com/iashin/poetry/controller/TestController.java
@@ -1,9 +1,14 @@
 package com.iashin.poetry.controller;
 
+import com.iashin.poetry.util.mail.MailUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @date: 2024/7/10
@@ -16,9 +21,20 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class TestController {
 
+    @Resource
+    private MailUtil mailUtil;
+
     @GetMapping
     public String ping() {
         log.info("pong!");
         return "pong";
+    }
+
+    @GetMapping("testMail")
+    public String testMail(){
+        List<String> emailList = new ArrayList<>();
+        emailList.add("iashin@aliyun.com");
+        mailUtil.sendMailMessage(emailList, "测试邮件", "你好,这是Poetry的测试邮件");
+        return "ok";
     }
 }
